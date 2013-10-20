75f3b7d86b3d030701315a483ee3f332c95e9153
lasinducharith <lasinducharith@gmail.com>
2013-10-20 23:48:19 +0530

    Making Link Generator working again

--- Configurator/src/com/weaved/utils/FileAndFolderNameList.java
+++ Configurator/src/com/weaved/utils/FileAndFolderNameList.java
@@ -9,9 +9,11 @@ package com.weaved.utils;
  * @author Thush
  */
 public class FileAndFolderNameList {
+
     public static String rootConfigFolder = "Config";
     public static String featureVecConfigFile = "feature_vectors_config.xml";
     public static String ikaslParamFile = "ikasl_params.xml";
-    public static String linkConfigFile = "link_config.xml"; 
-    
+    public static String linkConfigFile = "link_config_model.xml";
+    public static String importantPercepConfigFile = "important_percep_config.xml";
+    public static String perceptionConfigFile = "perception_config_model.xml";
 }
