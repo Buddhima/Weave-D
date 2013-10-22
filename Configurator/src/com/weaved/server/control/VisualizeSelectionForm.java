675c0a4d183d28cdae93d706621ee5e06f427df7
Buddhima <buddhimawijeweera@yahoo.com>
2013-10-22 16:37:41 +0530

    Visualizer Form modified

--- Configurator/src/com/weaved/server/control/VisualizeSelectionForm.java
+++ Configurator/src/com/weaved/server/control/VisualizeSelectionForm.java
@@ -56,7 +56,7 @@ public class VisualizeSelectionForm extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         fileList = new javax.swing.JComboBox();
-        DimentionRadioButton = new javax.swing.JRadioButton();
+        DimensionRadioButton = new javax.swing.JRadioButton();
         FeatureRadioButton = new javax.swing.JRadioButton();
         PerceptionRadioButton = new javax.swing.JRadioButton();
         StartButton = new javax.swing.JButton();
@@ -72,10 +72,10 @@ public class VisualizeSelectionForm extends javax.swing.JFrame {
 
         org.openide.awt.Mnemonics.setLocalizedText(jLabel2, org.openide.util.NbBundle.getMessage(VisualizeSelectionForm.class, "VisualizeSelectionForm.jLabel2.text")); // NOI18N
 
-        LevelRadioButtonGroup.add(DimentionRadioButton);
-        DimentionRadioButton.setSelected(true);
-        org.openide.awt.Mnemonics.setLocalizedText(DimentionRadioButton, org.openide.util.NbBundle.getMessage(VisualizeSelectionForm.class, "VisualizeSelectionForm.DimentionRadioButton.text")); // NOI18N
-        DimentionRadioButton.addActionListener(new java.awt.event.ActionListener() {
+        LevelRadioButtonGroup.add(DimensionRadioButton);
+        DimensionRadioButton.setSelected(true);
+        org.openide.awt.Mnemonics.setLocalizedText(DimensionRadioButton, org.openide.util.NbBundle.getMessage(VisualizeSelectionForm.class, "VisualizeSelectionForm.DimensionRadioButton.text")); // NOI18N
+        DimensionRadioButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 selectionChanged(evt);
             }
@@ -119,24 +119,22 @@ public class VisualizeSelectionForm extends javax.swing.JFrame {
                 .addGap(27, 27, 27)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
-                    .addGroup(jPanel1Layout.createSequentialGroup())
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(Cancel, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(StartButton, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(18, 18, 18)
-                                .addComponent(fileList, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(DimentionRadioButton)
-                                .addGap(18, 18, 18)
-                                .addComponent(FeatureRadioButton)
-                                .addGap(18, 18, 18)
-                                .addComponent(PerceptionRadioButton)))))
-                .addContainerGap(47, Short.MAX_VALUE))
+                            .addComponent(DimensionRadioButton)
+                            .addGap(18, 18, 18)
+                            .addComponent(FeatureRadioButton)
+                            .addGap(18, 18, 18)
+                            .addComponent(PerceptionRadioButton))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                            .addComponent(jLabel2)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(fileList, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addComponent(StartButton, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(18, 18, 18)
+                            .addComponent(Cancel, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(37, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -147,7 +145,7 @@ public class VisualizeSelectionForm extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(PerceptionRadioButton)
                     .addComponent(FeatureRadioButton)
-                    .addComponent(DimentionRadioButton))
+                    .addComponent(DimensionRadioButton))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(fileList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -216,7 +214,7 @@ public class VisualizeSelectionForm extends javax.swing.JFrame {
     }//GEN-LAST:event_selectionChanged
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Cancel;
-    private javax.swing.JRadioButton DimentionRadioButton;
+    private javax.swing.JRadioButton DimensionRadioButton;
     private javax.swing.JRadioButton FeatureRadioButton;
     private javax.swing.ButtonGroup LevelRadioButtonGroup;
     private javax.swing.JRadioButton PerceptionRadioButton;
@@ -235,8 +233,8 @@ public class VisualizeSelectionForm extends javax.swing.JFrame {
     }
 
     private void changeCurrentPath() {
-        if (DimentionRadioButton.isSelected()) {
-            currentPath = basePath + "dimention\\";
+        if (DimensionRadioButton.isSelected()) {
+            currentPath = basePath + "dimension\\";
         } else if (FeatureRadioButton.isSelected()) {
             currentPath = basePath + "feature\\";
         } else if (PerceptionRadioButton.isSelected()) {
