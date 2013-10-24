6936be5901589451196e64f5d45295ad87ffff7f
lasinducharith <lasinducharith@gmail.com>
2013-10-24 13:39:25 +0530

    Link generator working and removed previous unused files inside Configurator

--- Configurator/src/com/weaved/main/WeavedMain.java
+++ Configurator/src/com/weaved/main/WeavedMain.java
@@ -34,7 +34,7 @@ public class WeavedMain {
     private LinkGeneratorConfigLoader linkConfigLoader;
     private IKASLConfigModel iKASLConfigModel;
     private LinkConfigModel linkConfigModel;
-    
+    private String OUTPUT_LOCATION = "Output";
     int counter;
     /*--------------------------------------------------------
      * Things to beware of
@@ -57,11 +57,11 @@ public class WeavedMain {
      */
     public void runIKASL() {
 
-        
-        
-        String currFileName = "input"+counter+".txt";
+
+
+        String currFileName = "input" + counter + ".txt";
         Boolean counterLessOrEqualThanFile = true;
-        
+
         ArrayList<IKASLConfigModelElement> iKASLConfigModelElements = getIKASLModelElementWithPrefix("L2", getiKASLConfigModel().getiKASLConfigModelElements());
         ArrayList<IKASLCompAllInputs> iKASLRuntimeHelpers = new ArrayList<IKASLCompAllInputs>();
         ArrayList<IKASLParams> paramList = new ArrayList<IKASLParams>();
@@ -76,31 +76,32 @@ public class WeavedMain {
             //and also find the highest number (i.e input1, input2,...)
             //in the given folder
             File dir = new File(featureVectorsConfigModelElement.getFeatureVectorLocation());
-            File[] files = dir.listFiles(new FilenameFilter() { 
-    	         public boolean accept(File dir, String filename)
-    	              { return filename.endsWith(".txt"); }     
+            File[] files = dir.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String filename) {
+                    return filename.endsWith(".txt");
+                }
             });
             ArrayList<Integer> inputFileNumbers = new ArrayList<Integer>();
-            for(File f : files){
+            for (File f : files) {
                 String fName = f.getName();
-                if(fName.startsWith("input")){
+                if (fName.startsWith("input")) {
                     int idx = fName.indexOf(".");
-                    fName = fName.substring(5,idx);
+                    fName = fName.substring(5, idx);
                     inputFileNumbers.add(Integer.parseInt(fName));
                 }
             }
-            
+
             int maxFNumber = Collections.max(inputFileNumbers);
             //check whether counter of the learnt input files is less or equal
             //than the actual files in the folder
-            if(counter>maxFNumber){
-                counterLessOrEqualThanFile=false;
+            if (counter > maxFNumber) {
+                counterLessOrEqualThanFile = false;
             }
-            
-            if(counterLessOrEqualThanFile){
+
+            if (counterLessOrEqualThanFile) {
                 //parse the file with the correct input file
-                parser.parseInput(featureVectorsConfigModelElement.getFeatureVectorLocation()+ File.separator + currFileName);
-            
+                parser.parseInput(featureVectorsConfigModelElement.getFeatureVectorLocation() + File.separator + currFileName);
+
                 IKASLCompAllInputs iKASLRuntimeHelper = new IKASLCompAllInputs();
                 iKASLRuntimeHelper.setiWeights(parser.getiWeights());
                 iKASLRuntimeHelper.setiNames(parser.getiNames());
@@ -126,14 +127,14 @@ public class WeavedMain {
             }
         }
 
-        if(counter == 1){
+        if (counter == 1) {
             getPercpModelFacade().createIKASLComponents(paramList.size(), paramList, idList);
         }
 
-        if(counterLessOrEqualThanFile){
+        if (counterLessOrEqualThanFile) {
             for (IKASLCompAllInputs helper : iKASLRuntimeHelpers) {
                 getPercpModelFacade().runIKASLTest(helper.getStackId(), helper.getiKASLParams(),
-                    helper.getiWeights(), helper.getiNames(), helper.getMin(), helper.getMax(), helper.getDimension());
+                        helper.getiWeights(), helper.getiNames(), helper.getMin(), helper.getMax(), helper.getDimension());
             }
             //increment the counter so the program will read the next input file, when we click
             //'learn incrementally' button next.
@@ -142,12 +143,18 @@ public class WeavedMain {
     }
 
     /**
-     * Run the link generation for required IKASL components
-     * TODO: It's not just LOF1 and L0F2, therefore we've to find between which 
-     * IKASL components to generate these links (by reading necessary xml files)
-     * and generate links dynamically
+     * Run the link generation for required IKASL components TODO: It's not just
+     * LOF1 and L0F2, therefore we've to find between which IKASL components to
+     * generate these links (by reading necessary xml files) and generate links
+     * dynamically
      */
     public void runLinkGenerator() {
+
+        //TODO: Need to write code for loading the link configuration xml
+        //TODO: Need to check whether link config model/link config loader classes are working properly
+        linkConfigLoader = new LinkGeneratorConfigLoader();
+        linkConfigLoader.loadConfig(FileAndFolderNameList.rootConfigFolder + File.separator + FileAndFolderNameList.linkConfigFile);
+        linkConfigModel = (LinkConfigModel) linkConfigLoader.getPopulatedConfigModel();
         
         //=================== OBSOLETE CODE ==========================================
         //create only cross feature links, no temporal links
@@ -157,9 +164,9 @@ public class WeavedMain {
         //First, find all the cross feature and temporal links that needs to be created
         //Assumption: We create temporal links for all the IKASLs
         //TODO: Check whether temporal links are workng properly
-        for(String link : linkConfigModel.getCrossLinks()){
-            String[] items = link.split(",");
-            getPercpModelFacade().runLinkGeneration(items[0], items[1], true, true);
+        for (String link : linkConfigModel.getCrossLinks()) {
+            String[] items = link.split("-");
+            getPercpModelFacade().runLinkGeneration(OUTPUT_LOCATION + File.separator + items[0], OUTPUT_LOCATION + File.separator + items[1], true, true);
         }
     }
 
@@ -171,7 +178,7 @@ public class WeavedMain {
     }
 
     public void LoadConfigurations() {
-        
+
         String configFolder = FileAndFolderNameList.rootConfigFolder;
         featureVectorsConfigLoader = new FeatureVectorsConfigLoader();
         featureVectorsConfigLoader.loadConfig(configFolder + File.separator + FileAndFolderNameList.featureVecConfigFile);
@@ -181,25 +188,21 @@ public class WeavedMain {
         iKASLConfigLoader.loadConfig(configFolder + File.separator + FileAndFolderNameList.ikaslParamFile);
         setiKASLConfigModel((IKASLConfigModel) iKASLConfigLoader.getPopulatedConfigModel());
 
-        //TODO: Need to write code for loading the link configuration xml
-        //TODO: Need to check whether link config model/link config loader classes are working properly
-        linkConfigLoader = new LinkGeneratorConfigLoader();
-        linkConfigLoader.loadConfig(configFolder + File.separator + FileAndFolderNameList.linkConfigFile);
-        linkConfigModel = (LinkConfigModel) linkConfigLoader.getPopulatedConfigModel();
-        
+
     }
 
     /**
-     * Returns the IKASLConfigModelElements with the given prefix. E.g. if the prefix is,
-     * L2 - Return all dimension level IKASLConfigModelElements
-     * L1 - Return all feature level IKASLConfigModelElements
-     * L0 - Return all perception level IKASLConfigModelElements
-     * 
+     * Returns the IKASLConfigModelElements with the given prefix. E.g. if the
+     * prefix is, L2 - Return all dimension level IKASLConfigModelElements L1 -
+     * Return all feature level IKASLConfigModelElements L0 - Return all
+     * perception level IKASLConfigModelElements
+     *
      * TODO: Write What does this method do?
+     *
      * @param prefix First part of IKASL ID (e.g. L0/L1/L2)
-     * @param iKASLConfigModelElements Arraylist of IKASLConfigModelElements loaded to WeaveDMain 
-     * by the corresponding config loader
-     * @return 
+     * @param iKASLConfigModelElements Arraylist of IKASLConfigModelElements
+     * loaded to WeaveDMain by the corresponding config loader
+     * @return
      */
     private ArrayList<IKASLConfigModelElement> getIKASLModelElementWithPrefix(String prefix, ArrayList<IKASLConfigModelElement> iKASLConfigModelElements) {
 
@@ -214,10 +217,12 @@ public class WeavedMain {
     }
 
     /**
-     * TODO: Write a more intuitive method name. This name doesn't explain "correspond to what?"
+     * TODO: Write a more intuitive method name. This name doesn't explain
+     * "correspond to what?"
+     *
      * @param iKASLConfigModelElement
      * @param featureVectorsConfigModelElements
-     * @return 
+     * @return
      */
     private FeatureVectorsConfigModelElement getCorrespondingFeatureVectoreElement(IKASLConfigModelElement iKASLConfigModelElement, ArrayList<FeatureVectorsConfigModelElement> featureVectorsConfigModelElements) {
 
