4f40cbb60a6a45c5bd91f871b0fa23719375b0c5
thushv89 <thushv@gmail.com>
2013-10-28 08:17:41 +0530

    Fixed lasindu's last commit error. Tried to add a new menu item to Edit menu (Reset WeaveD). Still not working

---
+++ Configurator/src/com/weaved/server/configurator/menu/ResetWeaveDMenuItemAction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.weaved.server.configurator.menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Thush
+ */
+public class ResetWeaveDMenuItemAction implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
